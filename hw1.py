edd8988c321ee29543f7935ffe277f2c9fef0c3e
mobi7890 <mobi7890@yahoo.com>
2015-03-26 21:53:50 +0100

    Add notes

--- hw1.py
+++
@@ -1,3 +0,0 @@
-This is a test for git repository
-The second line
-try push
---
+++ python_note.py
@@ -0,0 +1,11 @@
+Variables, expressions and statements
+type()
+
+Python’s keywords
+and del from not while as elif global 
+or with assert else if pass yield break
+except import print class exec in raise 
+continue finally is return def for lambda try
+
+Modulus operator'%'(works on integers and yield remainder)
+
